95f7fcb293333fdb99d306f165529c7f421f80a2
javad sajjadi <64535464+javadsajjadi@users.noreply.github.com>
2022-12-16 16:56:50 +0330

    Update lightbox.js

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -139,25 +139,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
-
-  $(document).ready(function () {
-
-    ConvertNumberToPersion();
-});
-
-function ConvertNumberToPersion() {
-    persian = { 0: '۰', ۱: '۱', ۲: '۲', ۳: '۳', ۴: '۴', ۵: '۵', ۶: '۶', ۷: '۷', ۸: '۸', ۹: '۹' };
-    function traverse(el) {
-        if (el.nodeType == 3) {
-            var list = el.data.match(/[0-9]/g);
-            if (list != null && list.length != 0) {
-                for (var i = 0; i < list.length; i++)
-                    el.data = el.data.replace(list[i], persian[list[i]]);
-            }
-        }
-        for (var i = 0; i < el.childNodes.length; i++) {
-            traverse(el.childNodes[i]);
-        }
-    }
-    traverse(document.body);
-}
