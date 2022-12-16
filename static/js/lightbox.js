dad515ff473608598ca8a78f888c07323d21921f
javad sajjadi <64535464+javadsajjadi@users.noreply.github.com>
2022-12-16 16:35:19 +0330

    Update lightbox.js

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -138,10 +138,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+
+});
 var replaceDigits = function() {
         var map = ["&\#1776;","&\#1777;","&\#1778;","&\#1779;","&\#1780;","&\#1781;","&\#1782;","&\#1783;","&\#1784;","&\#1785;"]
         document.body.innerHTML = document.body.innerHTML.replace(/\d(?=[^<>]*(<|$))/g, function($0) { return map[$0]});
     }
 window.onload = replaceDigits;
     }
-});
